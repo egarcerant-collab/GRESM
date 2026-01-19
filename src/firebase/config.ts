01636b4ca2d73bad9a0e91bdeef819a238e2375d
EDUARDO GARCERANT <egarcerant@dusakawiepsi.com>
2026-01-19 19:57:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9747359097-37055",
+  "appId": "1:40247950279:web:546d498aa0c94b17f2b7e2",
+  "apiKey": "AIzaSyBQbRf0K1rJcc3tydNQce1IX7LVFEJCPac",
+  "authDomain": "studio-9747359097-37055.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "40247950279"
+};
