dc23afb02c133bcf209c5db2ac5226a767ffbb3f
EDUARDO GARCERANT <egarcerant@dusakawiepsi.com>
2026-02-10 22:38:00 +0000

    Cannot read properties of undefined (reading 'trimEnd') que opasa con es

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,9 +4,9 @@
 
 export const firebaseConfig = {
   apiKey: 'AIzaSyBQbRf0K1rJcc3tydNQce1IX7LVFEJCPac',
-  authDomain: 'studio-9747359097-37055.firebaseapp.com',
-  projectId: 'studio-9747359097-37055',
-  storageBucket: 'studio-9747359097-37055.appspot.com',
+  authDomain: 'studio-97473590-37055.firebaseapp.com',
+  projectId: 'studio-97473590-37055',
+  storageBucket: 'studio-97473590-37055.appspot.com',
   messagingSenderId: '40247950279',
   appId: '1:40247950279:web:546d498aa0c94b17f2b7e2',
 };
