21d45c6b3733d051948d8f68cfb8433c69c7dd20
EDUARDO GARCERANT <egarcerant@dusakawiepsi.com>
2026-02-11 22:15:44 +0000

    cul es el berraco promblema con esto

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,10 +3,10 @@
 // these should be managed via environment variables.
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyBQbRf0K1rJcc3tydNQce1IX7LVFEJCPac',
-  authDomain: 'studio-97473590-37055.firebaseapp.com',
-  projectId: 'studio-97473590-37055',
-  storageBucket: 'studio-97473590-37055.appspot.com',
-  messagingSenderId: '40247950279',
-  appId: '1:40247950279:web:546d498aa0c94b17f2b7e2',
+  apiKey: "AIzaSyBQbRf0K1rJcc3tydNQce1IX7LVFEJCPac",
+  authDomain: "studio-97473590-37055.firebaseapp.com",
+  projectId: "studio-97473590-37055",
+  storageBucket: "studio-97473590-37055.appspot.com",
+  messagingSenderId: "40247950279",
+  appId: "1:40247950279:web:546d498aa0c94b17f2b7e2",
 };
